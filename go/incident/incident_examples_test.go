42c175a74a051aeda0db8c6c4efab1d398e26bce
Mat Ryer <matryer@users.noreply.github.com>
2023-03-09 12:49:06 +0000

    added go example

---
+++ go/incident/incident_examples_test.go
@@ -0,0 +1,27 @@
+package incident_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/grafana/incident-api/go/incident"
+)
+
+// ExampleCreateIncident shows how to create an incident.
+// It uses the incident.NewTestClient so all responses are stubbed,
+// you should use incident.NewClient, specifying the API endpoint
+// and the service account token to send with the requests.
+func ExampleCreateIncident() {
+	ctx := context.Background()
+	client := incident.NewTestClient()
+	incidentsService := incident.NewIncidentsService(client)
+	createIncidentResp, err := incidentsService.CreateIncident(ctx, incident.CreateIncidentRequest{
+		Title: "high latency in web requests",
+	})
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+	fmt.Printf("new incident: %s\n", createIncidentResp.Incident.Title)
+	// Output: new incident: high latency in web requests
+}
